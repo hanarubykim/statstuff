8c5348eedd7e159f1ae3ac0f8045aa46fcf5b060
Hana Kim <hanarubykim2002@gmail.com>
2019-11-13 23:50:20 -0500

    doneish

--- main.c
+++ main.c
@@ -5,11 +5,16 @@
 int main(){
   struct stat st;
   stat("main.c", &st);
-  printf("The size of this file is %ld bytes\n\n", st.st_size);
+  printf("The size of this file is %ld\n\n bytes", st.st_size);
+  int i;
+  long tmp = st.st_size;
+  char * byteSize[] = {"B, KB, MB, GB"};
+  while(tmp >= 1024){
+    tmp /= 1024;
+    i++;
+  }
+  printf("More readable... The size of this file is %ld\n\n %s", tmp, byteSize[i]);
   printf("The mode of this file is %d\n\n", st.st_mode);
-  printf("The time this file was last accessed is %ld\n\n", st.st_atime);
+  printf("The time this file was last accessed is %s\n\n", ctime(&st.st_atime));
   printf("Permissions of this file: %d\n\n", st.st_mode);
-//Modify the size so it is printed out in human readable form (that is using B, KB, MB, GB).
-//You may want to look into the sprintf function to help.
-// Attempt to print out the permissions in ls -l form, ignoring the early bits that include the file type information
 }
